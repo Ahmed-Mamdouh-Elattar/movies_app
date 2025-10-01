91ff15a5dd4bb466019bfd685af2ed6858dbcd75
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2025-10-01 12:27:04 +0300

    add icons assets to the app

---
+++ lib/core/helper/assets.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for iconsBox
+  /// assets/icons/box.svg
+  static const String iconsBox = "assets/icons/box.svg";
+
+  /// Assets for iconsPopcorn
+  /// assets/icons/popcorn.svg
+  static const String iconsPopcorn = "assets/icons/popcorn.svg";
+
+  /// Assets for iconsSearch
+  /// assets/icons/search.svg
+  static const String iconsSearch = "assets/icons/search.svg";
+}
+
