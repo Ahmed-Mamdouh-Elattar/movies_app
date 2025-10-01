df998aebbb33fe85e01957c7e3407e04a962323a
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2025-10-01 12:16:40 +0300

    create app text style and make it responsive

---
+++ lib/core/config/app_text_style.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/widgets.dart';
+import 'package:movies_app/core/config/device_size.dart';
+
+class AppTextStyle {
+  static TextStyle semiBold18(BuildContext context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, baseFontSize: 18),
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle semiBold16(BuildContext context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, baseFontSize: 16),
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle semiBold12(BuildContext context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, baseFontSize: 12),
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle medium14(BuildContext context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, baseFontSize: 14),
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle medium12(BuildContext context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, baseFontSize: 12),
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle regular14(BuildContext context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, baseFontSize: 14),
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle regular12(BuildContext context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, baseFontSize: 12),
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle regular16(BuildContext context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, baseFontSize: 16),
+      fontWeight: FontWeight.w400,
+    );
+  }
+}
+
+double getResponsiveFontSize(
+  BuildContext context, {
+  required double baseFontSize,
+}) {
+  double scaleFactor = getScaleFactor(context);
+  double responsiveFontSize = baseFontSize * scaleFactor;
+  double lowerLimit = baseFontSize * 0.8;
+  double upperLimit = baseFontSize * 1.2;
+  return responsiveFontSize.clamp(lowerLimit, upperLimit);
+}
+
+double getScaleFactor(BuildContext context) {
+  double width = MediaQuery.sizeOf(context).width;
+  if (width < DeviceSize.tablet) {
+    return width / 550;
+  } else if (width < DeviceSize.desktob) {
+    return width / 900;
+  } else {
+    return width / 1400;
+  }
+}
---
+++ lib/core/config/device_size.dart
@@ -0,0 +1,4 @@
+class DeviceSize {
+  static const double desktob = 1100;
+  static const double tablet = 800;
+}
