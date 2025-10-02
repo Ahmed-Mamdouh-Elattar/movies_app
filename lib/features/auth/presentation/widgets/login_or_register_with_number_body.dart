5c1c564b27c2e0f517e9ab45220e081d19e75e1a
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2025-10-02 12:02:41 +0300

    initiate login ore rgister with number page

---
+++ lib/features/auth/presentation/widgets/login_or_register_with_number_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class LoginOrRegisterWithNumberBody extends StatelessWidget {
+  const LoginOrRegisterWithNumberBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
