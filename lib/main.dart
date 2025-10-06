ef1a6ec2540d4f844f64ff4449741207b3347bdc
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2025-10-06 19:06:02 +0300

    call service locator in main

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:movies_app/core/config/app_theme.dart';
 import 'package:movies_app/core/routing/app_router.dart';
+import 'package:movies_app/core/services/service_locator.dart';
 import 'package:movies_app/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  setUpServiceLocator();
   runApp(const MoviesApp());
 }
 
