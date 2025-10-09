4aabb930824a382a4389ac4a5d2d21926c3ae74e
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2025-10-09 14:32:15 +0300

    changing service locator

--- lib/core/services/service_locator.dart
+++ lib/core/services/service_locator.dart
@@ -1,5 +1,7 @@
 import 'package:get_it/get_it.dart';
+import 'package:movies_app/features/auth/data/repositories/facebook_auth_repo_impl.dart';
 import 'package:movies_app/features/auth/data/repositories/google_auth_repo_imp.dart';
+import 'package:movies_app/features/auth/domain/usecases/sign_in_or_up_with_facebook_use_case.dart';
 import 'package:movies_app/features/auth/domain/usecases/sign_in_or_up_with_google_use_case.dart';
 import 'package:movies_app/features/auth/presentation/cubit/auth_cubit.dart';
 
@@ -9,7 +11,16 @@ void setUpServiceLocator() {
   getIt.registerLazySingleton<SignInOrUpWithGoogleUseCase>(
     () => SignInOrUpWithGoogleUseCase(getIt<GoogleAuthRepoImp>()),
   );
+  getIt.registerLazySingleton<FacebookAuthRepoImpl>(
+    () => FacebookAuthRepoImpl(),
+  );
+  getIt.registerLazySingleton<SignInOrUpWithFacebookUseCase>(
+    () => SignInOrUpWithFacebookUseCase(getIt<FacebookAuthRepoImpl>()),
+  );
   getIt.registerFactory<AuthCubit>(
-    () => AuthCubit(getIt<SignInOrUpWithGoogleUseCase>()),
+    () => AuthCubit(
+      getIt<SignInOrUpWithGoogleUseCase>(),
+      getIt<SignInOrUpWithFacebookUseCase>(),
+    ),
   );
 }
