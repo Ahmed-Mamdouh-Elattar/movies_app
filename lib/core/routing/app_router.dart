133a477c7a046edd8165b8baea57667c970e76bd
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2025-10-15 18:20:38 +0300

    provide the cubit

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -2,10 +2,12 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:movies_app/core/routing/page_name.dart';
 import 'package:movies_app/core/services/service_locator.dart';
-import 'package:movies_app/features/auth/presentation/cubit/auth_cubit.dart';
+import 'package:movies_app/features/auth/presentation/managers/phone_auth_cubit/phone_auth_cubit.dart';
+import 'package:movies_app/features/auth/presentation/managers/social_auth_cubit/social_auth_cubit.dart';
 import 'package:movies_app/features/auth/presentation/pages/login_page.dart';
-import 'package:movies_app/features/auth/presentation/pages/phone_auth_page.dart';
 import 'package:movies_app/features/auth/presentation/pages/otp_verification_page.dart';
+import 'package:movies_app/features/auth/presentation/pages/phone_auth_page.dart';
+
 import 'package:movies_app/features/auth/presentation/pages/register_page.dart';
 import 'package:movies_app/features/splash_screen/presentation/pages/splash_page.dart';
 
@@ -16,8 +18,10 @@ final appRouter = GoRouter(
       builder: (context, state) => const SplashPage(),
     ),
     ShellRoute(
-      builder: (context, state, child) =>
-          BlocProvider(create: (context) => getIt<AuthCubit>(), child: child),
+      builder: (context, state, child) => BlocProvider(
+        create: (context) => getIt<SocialAuthCubit>(),
+        child: child,
+      ),
       routes: [
         GoRoute(
           path: PageName.login,
@@ -27,13 +31,22 @@ final appRouter = GoRouter(
               path: 'register',
               builder: (context, state) => const RegisterPage(),
             ),
-            GoRoute(
-              path: 'phone-auth',
-              builder: (context, state) => const PhoneAuthPage(),
+            ShellRoute(
+              builder: (context, state, child) => BlocProvider(
+                create: (context) => getIt<PhoneAuthCubit>(),
+                child: child,
+              ),
+
               routes: [
                 GoRoute(
-                  path: 'otp-verification',
-                  builder: (context, state) => const OtpVerificationPage(),
+                  path: 'phone-auth',
+                  builder: (context, state) => const PhoneAuthPage(),
+                  routes: [
+                    GoRoute(
+                      path: 'otp-verification',
+                      builder: (context, state) => const OtpVerificationPage(),
+                    ),
+                  ],
                 ),
               ],
             ),
