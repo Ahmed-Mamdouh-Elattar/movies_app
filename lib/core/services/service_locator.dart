e0810e86db3eafc184e7649c56ae2f6908e28996
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2025-10-18 18:31:17 +0300

    setup service locator for auth cubit and use cases and auth repo impl

--- lib/core/services/service_locator.dart
+++ lib/core/services/service_locator.dart
@@ -1,13 +1,19 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
+import 'package:movies_app/features/auth/data/repositories/auth_repo_impl.dart';
 import 'package:movies_app/features/auth/data/repositories/facebook_auth_repo_impl.dart';
 import 'package:movies_app/features/auth/data/repositories/google_auth_repo_imp.dart';
 import 'package:movies_app/features/auth/data/repositories/phone_auth_repo_impl.dart';
+import 'package:movies_app/features/auth/domain/usecases/get_auth_state_use_case.dart';
 import 'package:movies_app/features/auth/domain/usecases/send_code_use_case.dart';
 import 'package:movies_app/features/auth/domain/usecases/sign_in_or_up_with_facebook_use_case.dart';
 import 'package:movies_app/features/auth/domain/usecases/sign_in_or_up_with_google_use_case.dart';
+import 'package:movies_app/features/auth/domain/usecases/sign_out_use_case.dart';
 import 'package:movies_app/features/auth/domain/usecases/verify_code_use_case.dart';
+import 'package:movies_app/features/auth/presentation/managers/auth_cubit/auth_cubit.dart';
 import 'package:movies_app/features/auth/presentation/managers/phone_auth_cubit/phone_auth_cubit.dart';
 import 'package:movies_app/features/auth/presentation/managers/social_auth_cubit/social_auth_cubit.dart';
+import 'package:movies_app/features/splash_screen/managers/cubit/splash_cubit.dart';
 
 final getIt = GetIt.instance;
 void setUpServiceLocator() {
@@ -40,4 +46,17 @@ void setUpServiceLocator() {
       verifyCodeUseCase: getIt<VerifyCodeUseCase>(),
     ),
   );
+  getIt.registerLazySingleton<AuthRepoImpl>(
+    () => AuthRepoImpl(FirebaseAuth.instance),
+  );
+  getIt.registerLazySingleton<GetAuthStateUseCase>(
+    () => GetAuthStateUseCase(getIt<AuthRepoImpl>()),
+  );
+  getIt.registerLazySingleton<SignOutUseCase>(
+    () => SignOutUseCase(getIt<AuthRepoImpl>()),
+  );
+  getIt.registerFactory(
+    () => AuthCubit(getIt<GetAuthStateUseCase>(), getIt<SignOutUseCase>()),
+  );
+  getIt.registerFactory(() => SplashCubit());
 }
