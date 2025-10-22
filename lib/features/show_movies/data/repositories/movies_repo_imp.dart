5251ce4cc1a6cfbc9551efb21769a4b6d976ebcd
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2025-10-22 05:42:36 +0300

    create movies repo and repo imp

---
+++ lib/features/show_movies/data/repositories/movies_repo_imp.dart
@@ -0,0 +1,29 @@
+import 'package:dio/dio.dart';
+import 'package:movies_app/core/errors/failures.dart';
+import 'package:movies_app/core/errors/server_failure.dart';
+import 'package:movies_app/core/utils/result.dart';
+import 'package:movies_app/features/show_movies/data/datasources/movies_data_source.dart';
+import 'package:movies_app/features/show_movies/domain/entities/movies_entity.dart';
+import 'package:movies_app/features/show_movies/domain/repositories/movies_repo.dart';
+
+class MoviesRepoImp implements MoviesRepo {
+  final MoviesDataSource _moviesDataSource;
+  MoviesRepoImp(this._moviesDataSource);
+  @override
+  Future<Result<List<MoviesEntity>>> getMoviesByCategory({
+    required String category,
+    required int page,
+  }) async {
+    try {
+      final result = await _moviesDataSource.getMoviesByCategory(
+        category: category,
+        page: page,
+      );
+      return Result.success(result.toEntity());
+    } on DioException catch (e) {
+      return Result.failure(ServerFailure.fromDioError(e));
+    } on Exception {
+      return const Result.failure(Failures(errMessage: "Unexpected error"));
+    }
+  }
+}
---
+++ lib/features/show_movies/domain/repositories/movies_repo.dart
@@ -0,0 +1,9 @@
+import 'package:movies_app/core/utils/result.dart';
+import 'package:movies_app/features/show_movies/domain/entities/movies_entity.dart';
+
+abstract class MoviesRepo {
+  Future<Result<List<MoviesEntity>>> getMoviesByCategory({
+    required String category,
+    required int page,
+  });
+}
