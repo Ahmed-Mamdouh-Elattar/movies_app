0c0f656c1dcd96cf42fb0255a7cf1a2b1cdc11f8
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2025-10-22 05:42:07 +0300

    create movies entity

--- lib/core/helper/constants.dart
+++ lib/core/helper/constants.dart
@@ -1,1 +1,2 @@
 const kPadding = 20.0;
+const kBaseImageURL = "https://image.tmdb.org/t/p/original";
---
+++ lib/features/show_movies/domain/entities/movies_entity.dart
@@ -0,0 +1,7 @@
+class MoviesEntity {
+  final int id;
+
+  final String poster;
+
+  const MoviesEntity({required this.id, required this.poster});
+}
