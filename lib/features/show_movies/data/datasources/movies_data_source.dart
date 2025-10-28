9258d855643e0ba8de9e5a3c30f00c2cf39cef1b
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2025-10-28 05:39:15 +0300

    call three method in movie data source

--- lib/features/show_movies/data/datasources/movies_data_source.dart
+++ lib/features/show_movies/data/datasources/movies_data_source.dart
@@ -1,4 +1,7 @@
 import 'package:movies_app/features/show_movies/data/datasources/api_service.dart';
+import 'package:movies_app/features/show_movies/data/models/movie_cast_model/movie_cast_model.dart';
+import 'package:movies_app/features/show_movies/data/models/movie_details_model/movie_details_model.dart';
+import 'package:movies_app/features/show_movies/data/models/movie_reviews_model/movie_reviews_model.dart';
 import 'package:movies_app/features/show_movies/data/models/movies_model/movies_model.dart';
 
 abstract class MoviesDataSource {
@@ -7,6 +10,9 @@ abstract class MoviesDataSource {
     required int page,
   });
   Future<MoviesModel> getRandomeMovies({required int page});
+  Future<MovieDetailsModel> getMovieDetails({required int movieId});
+  Future<MovieCastModel> getMovieCast({required int movieId});
+  Future<MovieReviewsModel> getMovieReviews({required int movieId});
 }
 
 class MoviesDataSourceImpl implements MoviesDataSource {
@@ -31,6 +37,33 @@ class MoviesDataSourceImpl implements MoviesDataSource {
   Future<MoviesModel> getRandomeMovies({required int page}) async {
     try {
       return await _apiService.getRandomeMovies(page: page);
+    } on Exception {
+      rethrow;
+    }
+  }
+
+  @override
+  Future<MovieCastModel> getMovieCast({required int movieId}) async {
+    try {
+      return await _apiService.getMovieCast(movieId: movieId);
+    } on Exception {
+      rethrow;
+    }
+  }
+
+  @override
+  Future<MovieDetailsModel> getMovieDetails({required int movieId}) async {
+    try {
+      return await _apiService.getMovieDetails(movieId: movieId);
+    } on Exception {
+      rethrow;
+    }
+  }
+
+  @override
+  Future<MovieReviewsModel> getMovieReviews({required int movieId}) async {
+    try {
+      return await _apiService.getMovieReviews(movieId: movieId);
     } on Exception {
       rethrow;
     }
