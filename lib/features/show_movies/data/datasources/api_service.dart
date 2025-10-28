c76afe78b705bbb91086b23aac81cdc03a6a38f8
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2025-10-28 05:37:39 +0300

    create 3 method first to get movie details,secnd to get movie reviews and third to get movie cast

--- lib/features/show_movies/data/datasources/api_service.dart
+++ lib/features/show_movies/data/datasources/api_service.dart
@@ -1,4 +1,7 @@
 import 'package:dio/dio.dart';
+import 'package:movies_app/features/show_movies/data/models/movie_cast_model/movie_cast_model.dart';
+import 'package:movies_app/features/show_movies/data/models/movie_details_model/movie_details_model.dart';
+import 'package:movies_app/features/show_movies/data/models/movie_reviews_model/movie_reviews_model.dart';
 import 'package:movies_app/features/show_movies/data/models/movies_model/movies_model.dart';
 import 'package:retrofit/error_logger.dart';
 import 'package:retrofit/http.dart';
@@ -18,6 +21,22 @@ abstract class ApiService {
   @GET("discover/movie")
   Future<MoviesModel> getRandomeMovies({
     @Query("page") required int page,
+    @Query("language") String language = "en-US",
+  });
+
+  @GET("movie/{movie_id}")
+  Future<MovieDetailsModel> getMovieDetails({
+    @Path("movie_id") required int movieId,
+    @Query("language") String language = "en-US",
+  });
+  @GET("movie/{movie_id}/credits")
+  Future<MovieCastModel> getMovieCast({
+    @Path("movie_id") required int movieId,
+    @Query("language") String language = "en-US",
+  });
+  @GET("movie/{movie_id}/reviews")
+  Future<MovieReviewsModel> getMovieReviews({
+    @Path("movie_id") required int movieId,
     @Query("language") String language = "en-US",
   });
 }
--- lib/features/show_movies/data/datasources/api_service.g.dart
+++ lib/features/show_movies/data/datasources/api_service.g.dart
@@ -88,6 +88,96 @@ class _ApiService implements ApiService {
     return _value;
   }
 
+  @override
+  Future<MovieDetailsModel> getMovieDetails({
+    required int movieId,
+    String language = "en-US",
+  }) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{r'language': language};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<MovieDetailsModel>(
+      Options(method: 'GET', headers: _headers, extra: _extra)
+          .compose(
+            _dio.options,
+            'movie/${movieId}',
+            queryParameters: queryParameters,
+            data: _data,
+          )
+          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+    );
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late MovieDetailsModel _value;
+    try {
+      _value = MovieDetailsModel.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
+  @override
+  Future<MovieCastModel> getMovieCast({
+    required int movieId,
+    String language = "en-US",
+  }) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{r'language': language};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<MovieCastModel>(
+      Options(method: 'GET', headers: _headers, extra: _extra)
+          .compose(
+            _dio.options,
+            'movie/${movieId}/credits',
+            queryParameters: queryParameters,
+            data: _data,
+          )
+          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+    );
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late MovieCastModel _value;
+    try {
+      _value = MovieCastModel.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
+  @override
+  Future<MovieReviewsModel> getMovieReviews({
+    required int movieId,
+    String language = "en-US",
+  }) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{r'language': language};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<MovieReviewsModel>(
+      Options(method: 'GET', headers: _headers, extra: _extra)
+          .compose(
+            _dio.options,
+            'movie/${movieId}/reviews',
+            queryParameters: queryParameters,
+            data: _data,
+          )
+          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+    );
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late MovieReviewsModel _value;
+    try {
+      _value = MovieReviewsModel.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
